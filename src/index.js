96fc8cde49178f09f8f15f213e0530d0f42bfc80
imcnb <chaminduherath329@gmail.com>
2025-06-30 19:06:44 +0530

    debug

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import DebugBoundary from './components/DebugBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <DebugBoundary>
     <App />
+    </DebugBoundary>
   </BrowserRouter>
 );
